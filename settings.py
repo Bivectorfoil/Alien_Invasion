2893d85b5c984b52e37e2473cc27b1980ba2367a
Bivectorfoil <550282600@qq.com>
2017-01-20 16:55:08 +0800

    All settings

---
+++ settings.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*- 
+
+class Settings():
+	'''存储《外星人入侵》的所有设置的类'''
+
+	def __init__(self):
+		# 屏幕设置
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (255,255,255)#背景色
+
+		# 飞船的设置
+		self.ship_speed_factor = 2.5
+
+		
